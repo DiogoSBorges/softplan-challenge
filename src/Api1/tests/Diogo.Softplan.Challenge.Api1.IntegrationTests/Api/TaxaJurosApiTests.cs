5dc5b4f12ac99323111a22ba2d7f5824a4456d99
Diogo Sousa Borges <diogosousa2266@gmail.com>
2019-10-10 00:50:10 -0400

    Alteração no SDK dos projetos de testes para .net Core 2.2 e Implementação de teste de integração da API 1

---
+++ src/Api1/tests/Diogo.Softplan.Challenge.Api1.IntegrationTests/Api/TaxaJurosApiTests.cs
@@ -0,0 +1,34 @@
+﻿using Diogo.Softplan.Challenge.Api1.Api;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace Diogo.Softplan.Challenge.Api1.Integration.Tests.Api
+{
+    public class TaxaJurosApiTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public TaxaJurosApiTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async void Quando_obter_taxa_de_juros(string metodo)
+        {
+            var client = _factory.CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), "/api/taxaJuros");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
