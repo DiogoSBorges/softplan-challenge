5d62ceda9e3f57debabf66761e7d3d44bf6b52e0
Diogo Sousa Borges <diogosousa2266@gmail.com>
2019-10-11 08:29:21 -0400

    Implementação do restante de teste de integração da API2 - Calcular Juros Formatado e ShowMeTheCode

--- src/Api2/tests/Diogo.Softplan.Challenge.Api2.Integration.Tests/Api/CalcularJurosControllerTests.cs
+++ src/Api2/tests/Diogo.Softplan.Challenge.Api2.Integration.Tests/Api/CalcularJurosControllerTests.cs
@@ -52,5 +52,34 @@ namespace Diogo.Softplan.Challenge.Api2.Integration.Tests.Api
 
             result.Should().Be("105.1");
         }
+
+        [Theory]
+        [InlineData("GET", 100, 5)]
+        public async void Quando_calcular_taxa_juros_formatado(string metodo, decimal valorInicial, int meses)
+        {
+
+            var mock = new Mock<IApi1Service>();
+            mock.Setup(x => x.ObterTaxaDeJurosAsync()).Returns(Task.FromResult(0.01m));
+
+            var client = _factory.WithWebHostBuilder(hostbuilder =>
+            {
+                hostbuilder.ConfigureTestServices((services) =>
+                {
+                    services.AddSingleton<IApi1Service>(mock.Object);
+                });
+            })
+        .CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), $"/api/calcularJuros/formatado?valorInicial={valorInicial}&meses={meses}");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+
+            var result = await response.Content.ReadAsStringAsync();
+            result.Should().Be("105,10");
+        }
     }
 }
